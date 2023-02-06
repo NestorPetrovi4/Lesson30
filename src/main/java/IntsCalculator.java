48df4ce6e2e066ff246b28822cacc663c62ec8e7
Oleg Nesterov <oleg9988@rambler.ru>
2023-02-06 16:51:45 +0300

    New calc metod

--- src/main/java/IntsCalculator.java
+++ src/main/java/IntsCalculator.java
@@ -1,2 +1,40 @@
-public class IntsCalculator {
+public class IntsCalculator implements Ints {
+    protected final Calculator target;
+
+    public IntsCalculator() {
+        this.target = new Calculator();
+    }
+
+    public int executeFormula(int arg0, int arg1, Calculator.Operation operation){
+        Double d = target.newFormula()
+                .addOperand((double) arg0)
+                .addOperand((double) arg1)
+                .calculate(operation)
+                .result();
+        return (int)Math.round(d);
+    }
+
+    @Override
+    public int sum(int arg0, int arg1) {
+        return executeFormula(arg0, arg1, Calculator.Operation.SUM);
+    }
+
+    @Override
+    public int mult(int arg0, int arg1) {
+        return executeFormula(arg0, arg1, Calculator.Operation.MULT);
+    }
+
+    @Override
+    public int pow(int arg0, int arg1) {
+        return executeFormula(arg0, arg1, Calculator.Operation.POW);
+    }
+
+    @Override
+    public int sub(int arg0, int arg1) {
+        return executeFormula(arg0, arg1, Calculator.Operation.SUB);
+    }
+    @Override
+    public int div(int arg0, int arg1) {
+        return executeFormula(arg0, arg1, Calculator.Operation.DIV);
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
-        System.out.println(
-                calc.newFormula()
-                        .addOperand(5)
-                        .addOperand(15)
-                        .calculate(Calculator.Operation.MULT)
-                        .result()
-        );
+        Ints calc = new IntsCalculator();
+        System.out.println("Сумма = " + calc.sum(4, 33));
+        System.out.println("Умножение = " + calc.mult(4, 7));
+        System.out.println("Возведение в степень = " + calc.pow(3, 3));
+        System.out.println("Деление = " + calc.div(12, 6));
     }
 }
